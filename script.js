c18152433ab407e39a70de30d218bf843d3cfedb
Soumya Rao <78649084+V-SOUMYA@users.noreply.github.com>
2026-02-08 16:02:30 -0500

    Add functions to draw car and arrows in SVG

--- script.js
+++ script.js
@@ -32,3 +32,54 @@ function renderStep() {
   stepCount.innerText = `Step ${currentStep + 1} of ${steps.length}`;
   drawVisual(step.visual);
 }
+
+function clearSVG() {
+  while (svg.firstChild) {
+    svg.removeChild(svg.firstChild);
+  }
+}
+
+function drawCar(x, y, color) {
+  const car = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  car.setAttribute("x", x);
+  car.setAttribute("y", y);
+  car.setAttribute("width", 80);
+  car.setAttribute("height", 40);
+  car.setAttribute("fill", color);
+  svg.appendChild(car);
+}
+
+function drawArrow(x1, y1, x2, y2) {
+  const arrow = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  arrow.setAttribute("x1", x1);
+  arrow.setAttribute("y1", y1);
+  arrow.setAttribute("x2", x2);
+  arrow.setAttribute("y2", y2);
+  arrow.setAttribute("stroke", "black");
+  arrow.setAttribute("stroke-width", "4");
+  svg.appendChild(arrow);
+}
+
+function drawVisual(type) {
+  clearSVG();
+
+  if (type === "moving_car") {
+    drawCar(50, 130, "blue");
+    drawArrow(140, 150, 220, 150);
+  }
+
+  if (type === "collision") {
+    drawCar(200, 130, "blue");
+    drawCar(280, 130, "red");
+  }
+
+  if (type === "stopping") {
+    drawCar(200, 130, "blue");
+  }
+
+  if (type === "impulse") {
+    drawCar(200, 130, "blue");
+    drawArrow(200, 90, 200, 30); // force arrow
+  }
+}
+
